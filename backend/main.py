98137b9a3fd03a604b4083ced168741a17426894
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-02-13 12:31:16 +0000

    debug mode define in .env

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from app import app
-import random
+import random, os
 from dotenv import load_dotenv
 
 if __name__ == "__main__":
   load_dotenv('.env')
-  app.run(host='0.0.0.0', port=random.randint(2000,9000), debug=True)
+  # print(os.getenv('DEBUG_MODE'))
+  if os.getenv('DEBUG_MODE') == True:
+    app.run(host='0.0.0.0', port=random.randint(2000,9000), debug=os.environ['DEBUG_MODE'])
+  else:
+    app.run(host='0.0.0.0', port=5000, debug=os.environ['DEBUG_MODE'])
 
