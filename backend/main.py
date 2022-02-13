2d483913f4f1fc7b78f55f3129e3e188f12cf3a6
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-02-13 03:31:06 +0000

    read env file

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from app import app
 import random
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',
-		port=random.randint(2000,9000))
+  load_dotenv('.env')
+  app.run(host='0.0.0.0', port=random.randint(2000,9000), debug=True)
 
