4a403f8cf1cf8e66c81c35c27007cf23fbdb00ba
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-02-15 01:05:59 +0000

    refact db.py

--- backend/app/models/db.py
+++ backend/app/models/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 import uuid
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database/data.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
