d6e47eda1e021618e6bec0e6cdfa80df250a746d
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-02-16 17:18:49 +0000

    add banner main

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from dotenv import load_dotenv
+from time import sleep
 import random, os
 
+welcome = """
+  __  __                 _                                      _                _____   _   _                   
+ |  \/  |   __ _   ___  | |_    ___   _ __    ___    ___     __| |   ___        |  ___| (_) | |  _ __ ___    ___ 
+ | |\/| |  / _` | / __| | __|  / _ \ | '__|  / __|  / _ \   / _` |  / _ \       | |_    | | | | | '_ ` _ \  / __|
+ | |  | | | (_| | \__ \ | |_  |  __/ | |    | (__  | (_) | | (_| | |  __/       |  _|   | | | | | | | | | | \__ \\
+ |_|  |_|  \__,_| |___/  \__|  \___| |_|     \___|  \___/   \__,_|  \___|       |_|     |_| |_| |_| |_| |_| |___/\n"""
 
 if __name__ == "__main__":
   load_dotenv('.env')
+  os.system('cls' if os.name == 'nt' else 'clear')
+  print(welcome)
   from app import app
   if "True" in os.getenv('DEBUG_MODE'):
     app.run(host='0.0.0.0', port=random.randint(2000,9000), debug=os.environ['DEBUG_MODE'])
